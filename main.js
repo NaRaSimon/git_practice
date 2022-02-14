5a4708278f610f276ce082194b4dda074d46bb7b
NaRaSimon <--list>
2022-02-14 23:43:09 +0900

    0214

---
+++ main.js
@@ -0,0 +1,22 @@
+setTimeout(function () {}, 3000)
+
+
+
+const uesrAge = {
+  name: 'Naraya',
+  age: 33
+}
+
+const  userEmail = {
+  name: 'NaraSimon',
+  email: 'naraya1225@gmail.com'
+}
+
+const target = Object.assign(uesrAge,userEmail)
+console.log(uesrAge)
+console.log(userEmail)
+console.log(target === uesrAge)
+
+const a = {k:123}
+const b = {k:123}
+console.log(a === b)
